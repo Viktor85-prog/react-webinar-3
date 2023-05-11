08f56cc563bc4e8053d841c7e1ada670aa6eeb2d
Victor <bulkin_viktor@mail.ru>
2023-05-11 15:58:59 +0300

    add uniq code number

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+	this.counter = this.state.list.length; // Счетчик уникальных значений
   }
 
   /**
@@ -42,9 +43,10 @@ class Store {
    * Добавление новой записи
    */
   addItem() {
+	this.counter ++;
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, {code: this.counter, title: `Новая запись №${this.counter}`}]
     })
   };
 
