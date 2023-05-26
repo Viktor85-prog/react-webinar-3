fadc2ce8002b1dd59f06118d4e41cca93f1b7cd8
Victor <bulkin_viktor@mail.ru>
2023-05-26 22:40:53 +0300

    1

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -36,7 +36,7 @@ function Main() {
 
   return (
     <PageLayout>
-      <Head title='Магазин'/>
+      <Head title='Магазин1'/>
       <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount}
                   sum={select.sum}/>
       <List list={select.list} renderItem={renders.item}/>
